a72585e06a3754ea310a0fee45d6c825eae49a7e
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-09-24 15:56:25 -0300

    added get and setters to api cadastros

--- projeto_organiza/src/pages/api/cadastros.js
+++ projeto_organiza/src/pages/api/cadastros.js
@@ -7,7 +7,7 @@ var cadastros = [
       saldo: 5000,
       email:'example@com',
       entradas: [{valor:5600,data:'20/10/2022'}],
-      saidas: [{categoria:'farmácia',valor:500,data:'22/10/2022'},{categoria:'shopping',valor:100,data:'22/10/2022'}],
+      saidas: [{categoria:'farmácia',valor:500,data:'21/10/2022'},{categoria:'shopping',valor:100,data:'22/10/2022'}],
     },
     {
       nome: 'Maria Santos',
@@ -25,6 +25,9 @@ var cadastros = [
         var info = req.body
         cadastros.push(info);
         res.status(200).json(info);
+    }else if(req.method == 'GET' && req.query.cpf!=null){
+        var get = cadastros.filter(cadastro => cadastro.cpf==req.query.cpf)
+        res.status(200).json(get);
     }else if(req.method == 'GET'){
         res.status(200).json(cadastros);
     }
