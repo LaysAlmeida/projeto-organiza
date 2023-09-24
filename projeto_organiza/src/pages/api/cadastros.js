6c2946fa482f2b9a65859c199f36f8e1dc4cf3d6
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-09-24 13:01:54 -0300

    Made APIs and responsivity

---
+++ projeto_organiza/src/pages/api/cadastros.js
@@ -0,0 +1,23 @@
+
+export const cadastros = [
+    {
+      nome: 'João Silva',
+      senha: '1234',
+      cpf:'00000000000',
+      saldo: 5000,
+      email:'example@com',
+      entradas: [{valor:5600,data:'20/10/2022'}],
+      saidas: [{categoria:'farmácia',valor:500,data:'22/10/2022'},{categoria:'shopping',valor:100,data:'22/10/2022'}],
+    },
+    {
+      nome: 'Maria Santos',
+      senha: '5321',
+      cpf:'00000000002',
+      saldo: 400,
+      email:'example2@com',
+      entradas: [{valor:1000,data:'20/10/2022'}],
+      saidas: [{categoria:'farmácia',valor:500,data:'22/10/2022'},{categoria:'shopping',valor:100,data:'22/10/2022'}],
+    },
+  ];
+  
+  
