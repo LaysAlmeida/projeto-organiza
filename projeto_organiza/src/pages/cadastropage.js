b7816e2c28730496bc1ba07be4daae452e816f36
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-09-24 21:32:53 -0300

    made changes in responsive design

--- projeto_organiza/src/pages/cadastropage.js
+++ projeto_organiza/src/pages/cadastropage.js
@@ -1,12 +1,11 @@
 const cadastropage = () =>{
     return(
         <section class="bg-blue-800 dark:bg-gray-900 h-screen">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 md:flex-row">
             <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-white dark:text-gray-50">
-                <img class="w-5 h-15 mr-2" src="/icon.png" alt="logo"/>
-                Organiza
+                <img class="w-10 h-30 mr-2" src="/icon.png" alt="logo"/>
             </a>
-            <div class="w-full bg-blue-800 rounded-lg md:mt-0 sm:max-w-md xl:p-0 dark:bg-blue-950">
+            <div class="w-full bg-blue-800 rounded-lg md:mt-0 sm:max-w-md xl:p-0 dark:bg-blue-950 ">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
                         Crie uma conta
@@ -29,11 +28,9 @@ const cadastropage = () =>{
                             <input type="password" name="senha" id="senha" placeholder="Senha" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                             <input type="password" name="senha2" id="senha2" placeholder="Confirme sua senha" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                         </div>
-
-                        <button type="submit" onClick={(e) => getUser(e)} class=" w-full text-white bg-primary-600 ring-2 ring-white hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Entrar</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            <a href="/cadastropage" class="font-medium text-white hover:underline dark:text-blue-500">Abrir conta</a>
-                        </p>
+                        <div className="flex justify-center">
+                            <button type="submit" onClick={(e) => getUser(e)} class=" w-2/3 text-white bg-primary-600 ring-2 ring-white hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Próximo</button>
+                        </div>
                     </form>
                     <div className="flex justify-center">
                          <h1 className="w-full bg-red-600 text-white rounded-lg text-center sm:w-1/2 md:w-1/2"></h1>
