a1092bcd99e7cbe6f7549536838263bffba90508
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-09-24 21:38:16 -0300

    made some corrections in background

--- projeto_organiza/src/pages/cadastropage.js
+++ projeto_organiza/src/pages/cadastropage.js
@@ -29,7 +29,7 @@ const cadastropage = () =>{
                             <input type="password" name="senha2" id="senha2" placeholder="Confirme sua senha" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                         </div>
                         <div className="flex justify-center">
-                            <button type="submit" onClick={(e) => getUser(e)} class=" w-2/3 text-white bg-primary-600 ring-2 ring-white hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Próximo</button>
+                            <button type="submit" onClick={(e) => getUser(e)} class=" w-full md:w-2/3 text-white bg-primary-600 ring-2 ring-white hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Próximo</button>
                         </div>
                     </form>
                     <div className="flex justify-center">
