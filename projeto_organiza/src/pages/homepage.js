c51df416860baa0dc9653bedca8d35476381e604
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-09-27 19:33:17 -0300

    props made on homepage

--- projeto_organiza/src/pages/homepage.js
+++ projeto_organiza/src/pages/homepage.js
@@ -1,7 +1,22 @@
-const homepage = () =>{
+import { useRouter } from "next/router"
+
+export const getStaticProps = async () => {
+    const { query } = useRouter();
+    const res = await fetch('http://localhost:3000/api/cadastros?cpf='+query.cpf)
+    const data = await res.json()
+  
+      return {
+        props: {
+          cadastros: data,
+        }
+      }
+  }
+
+const homepage = props =>{
     return(
         <div>
             homepage
+            {props.cadastros.nome}
         </div>
     )
 }
