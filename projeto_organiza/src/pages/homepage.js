ac8bb313d350545e79982e4e1b5109a3019c7654
Ramon Gabriel <ramongtstudy@gmail.com>
2023-09-28 14:57:44 -0300

    Alterações

--- projeto_organiza/src/pages/homepage.js
+++ projeto_organiza/src/pages/homepage.js
@@ -1,12 +1,11 @@
 import Navbar from "@/components/Navbar";
 import BarChart from "@/components/BarChart";
 import RecentOrders from "@/components/RecentOrders";
-import Cards from "@/components/Cards";
 import React, { useState, useEffect } from 'react'
 import usuarioService from "./services/UsuarioService";
 import UsuarioService from "./services/UsuarioService";
 import { useRouter } from "next/router";
-
+import Cards from "@/components/Cards";
 
 export const getStaticProps = async () => {
   const res = await fetch('http://localhost:3000/api/cadastros')
@@ -36,23 +35,19 @@ export default function homepage(props) {
     <div className="bg-main">
       <Navbar />
       <Cards />
-      <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4 text-white">
+      <div className="flex flex-col h-screen bg-gradient-to-t from-blue-950 to-blue-600 text-white items-start justify-center font-mukta gap-14 mt-12 ml-12">
+        <h1 className="text-5xl"> Olá {cadastro.nome}!</h1>
+        <div>
+          <h2 className="text-base font-medium">Saldo atual:</h2>
+          <h3 className="text-5xl">R${cadastro.saldo},00</h3>
+        </div>
         <BarChart />
         <RecentOrders />
-        <div />
-        <div className="flex flex-col h-screen bg-gradient-to-t from-blue-950 to-blue-600 text-white items-start justify-start font-mukta gap-14 mt-12 ml-12">
-          <h1 className="text-5xl"> Olá {cadastro.nome}!</h1>
-          <div>
-            <h2 className="text-base font-medium">Saldo atual:</h2>
-            <h3 className="text-5xl">R${cadastro.saldo},00</h3>
-          </div>
-          <BarChart />
-          <div className="flex p-4 items-center flex-row justify-center">
-            <div className="lg:col-span-2 col-span-1 bg-white flex w-full border p-4 rounded-lg ">
-              <div className="flex flex-col w-full pb-4">
-                <p className="text-gray-600">Patrimônio</p>
-                <p className="text-2xl font-bold">{cadastro.patrimonio}</p>
-              </div>
+        <div className="flex p-4 items-center flex-row justify-center">
+          <div className="lg:col-span-2 col-span-1 bg-white flex w-full border p-4 rounded-lg ">
+            <div className="flex flex-col w-full pb-4">
+              <p className="text-gray-600">Patrimônio</p>
+              <p className="text-2xl font-bold">{cadastro.patrimonio}</p>
             </div>
           </div>
         </div>
