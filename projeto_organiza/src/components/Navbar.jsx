73a6c0850f3e59af1c443dc26ea2a541169d7ade
lays.almeida <lays.almeida@compasso.com.br>
2023-09-27 22:54:15 -0300

    editing navbar component

--- projeto_organiza/src/components/Navbar.jsx
+++ projeto_organiza/src/components/Navbar.jsx
@@ -14,97 +14,94 @@ const Navbar = () => {
     }
 
     return (
-<<<<<<< HEAD
         <nav className="fixed w-full h-24 shadow-xl bg-blue-navbar">
-=======
-    <header>
-        <nav className="w-full h-24 shadow-xl bg-white">
->>>>>>> 1c6955af2c56e2c4d3de121fe6e39ee4056dde6c
-            <div className="flex justify-between items-center h-full w-full px-4 2x1:px-16 text-black">
-                <Link href='/'>
-                    <Image
-                        src={Logo}
-                        alt='Logo'
-                        width='65'
-                        height='45'
-                        className='cursor-pointer'
-                        priority
-                    />
-                </Link>
-                <div className="hidden sm:flex">
-                    <ul className="hidden sm:flex">
+            <header>
+                <div className="w-full h-24 shadow-xl bg-white">
+                    <div className="flex justify-between items-center h-full w-full px-4 2x1:px-16 text-black">
                         <Link href='/'>
-                            <li className="ml-10 uppercase hover:border-b text-xl">Home</li>
+                            <Image
+                                src={Logo}
+                                alt='Logo'
+                                width='65'
+                                height='45'
+                                className='cursor-pointer'
+                                priority
+                            />
                         </Link>
-                        <Link href='/despesas'>
-                            <li className="ml-10 uppercase hover:border-b text-xl">Despesas</li>
-                        </Link>
-                        <Link href='/configuracao'>
-                            <li className="ml-10 uppercase hover:border-b text-xl">Configuração</li>
-                        </Link>
-                    </ul>
-                </div>
-                <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
-                    <AiOutlineMenu size={25} />
-                </div>
-            </div>
-            <div className={
-                menuOpen
-                    ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-                    : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
-            }
-            >
-                <div className="flex w-full items-center justify-end">
-                    <div onClick={handleNav} className="cursor-pointer text-black">
-                        <AiOutlineClose size={25} />
+                        <div className="hidden sm:flex">
+                            <ul className="hidden sm:flex">
+                                <Link href='/'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl">Home</li>
+                                </Link>
+                                <Link href='/despesas'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl">Despesas</li>
+                                </Link>
+                                <Link href='/configuracao'>
+                                    <li className="ml-10 uppercase hover:border-b text-xl">Configuração</li>
+                                </Link>
+                            </ul>
+                        </div>
+                        <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+                            <AiOutlineMenu size={25} />
+                        </div>
                     </div>
-                </div>
-                <div className="flex-col py-4 text-black">
-                    <ul>
+                    <div className={
+                        menuOpen
+                            ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                            : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                    }
+                        <div className="flex w-full items-center justify-end">
+                            <div onClick={handleNav} className="cursor-pointer text-black">
+                                <AiOutlineClose size={25} />
+                            </div>
+                        </div>
+                        <div className="flex-col py-4 text-black">
+                            <ul>
+                                <Link href='/'>
+                                    <li
+                                        onClick={() => setMenuOpen(false)}
+                                        className="py-4 cursor-pointer"
+                                    >
+                                        Home
+                                    </li>
+                                </Link>
+                                <Link href='/despesas'>
+                                    <li
+                                        onClick={() => setMenuOpen(false)}
+                                        className="py-4 cursor-pointer"
+                                    >
+                                        Despesas
+                                    </li>
+                                </Link>
+                                <Link href='/configuracao'>
+                                    <li
+                                        onClick={() => setMenuOpen(false)}
+                                        className="py-4 cursor-pointer"
+                                    >
+                                        Configuração
+                                    </li>
+                                </Link>
+                            </ul>
+                        </div>
+                        <div className="flex flex-row justify-around pt-10 items-center text-black">
+                            <AiOutlineInstagram size={30} className="cursor-pointer" />
+                            <AiOutlineTwitter size={30} className="cursor-pointer" />
+                            <AiOutlineFacebook size={30} className="cursor-pointer" />
+                        </div>
                         <Link href='/'>
-                            <li
-                                onClick={() => setMenuOpen(false)}
-                                className="py-4 cursor-pointer"
-                            >
-                                Home
-                            </li>
+                            <Image
+                                src={Logo}
+                                alt='Logo'
+                                width='65'
+                                height='45'
+                                className='fixed cursor-pointer pt-6 ml-14'
+                                priority
+                            />
                         </Link>
-                        <Link href='/despesas'>
-                            <li
-                                onClick={() => setMenuOpen(false)}
-                                className="py-4 cursor-pointer"
-                            >
-                                Despesas
-                            </li>
-                        </Link>
-                        <Link href='/configuracao'>
-                            <li
-                                onClick={() => setMenuOpen(false)}
-                                className="py-4 cursor-pointer"
-                            >
-                                Configuração
-                            </li>
-                        </Link>
-                    </ul>
-                </div>
-                <div className="flex flex-row justify-around pt-10 items-center text-black">
-                    <AiOutlineInstagram size={30} className="cursor-pointer" />
-                    <AiOutlineTwitter size={30} className="cursor-pointer" />
-                    <AiOutlineFacebook size={30} className="cursor-pointer" />
-                </div>
-                <Link href='/'>
-                    <Image
-                        src={Logo}
-                        alt='Logo'
-                        width='65'
-                        height='45'
-                        className='fixed cursor-pointer pt-6 ml-14'
-                        priority
-                    />
-                </Link>
-            </div>
+                    </div>
+                </header>
         </nav>
-        </header>
+
     )
 }
 
