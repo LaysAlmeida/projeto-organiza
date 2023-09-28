7fbd020b04d9d0dcae3132b0c97eea07eac807a8
Ramon Gabriel <ramongtstudy@gmail.com>
2023-09-28 14:09:14 -0300

    Adição da lista das ultimas movimentações

--- projeto_organiza/src/pages/homepage.js
+++ projeto_organiza/src/pages/homepage.js
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar";
 import BarChart from "@/components/BarChart";
 import RecentOrders from "@/components/RecentOrders";
+import Cards from "@/components/Cards";
 import React, { useState, useEffect } from 'react'
 import usuarioService from "./services/UsuarioService";
 import UsuarioService from "./services/UsuarioService";
 import { useRouter } from "next/router";
-import Cards from "@/components/Cards";
+
 
 export const getStaticProps = async () => {
   const res = await fetch('http://localhost:3000/api/cadastros')
@@ -19,26 +20,29 @@ export const getStaticProps = async () => {
 }
 
 export default function homepage(props) {
-  const router = useRouter();
-  const [cadastro, setCadastro] = useState('')
+  // const router = useRouter();
+  // const [cadastro, setCadastro] = useState('')
 
-  useEffect(() => {
-    if (router.isReady) {
-      const cpf = router.query.cpf;
-      var usuario = props.cadastros.filter(cadastro => cadastro.cpf == cpf)[0]
-      setCadastro(usuario)
-    }
-  }, [router.isReady])
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     const cpf = router.query.cpf;
+  //     var usuario = props.cadastros.filter(cadastro => cadastro.cpf == cpf)[0]
+  //     setCadastro(usuario)
+  //   }
+  // }, [router.isReady])
 
 
   return (
     <div >
       <Navbar />
-      <div className="flex flex-col h-screen bg-gradient-to-t from-blue-950 to-blue-600 text-white items-center justify-center">
+      <Cards />
+      {/* <div className="flex flex-col h-screen bg-gradient-to-t from-blue-950 to-blue-600 text-white items-center justify-center">
         <h1 className="">Olá {cadastro.nome}, seja bem vinda ao Organiza!</h1>
         <h2>Saldo: R${cadastro.saldo},00</h2>
-        <Cards />
+      </div> */}
+      <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4 text-white">
         <BarChart />
+        <RecentOrders />
       </div>
     </div>
   )
