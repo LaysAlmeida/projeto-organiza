e631f12eb54622222ce0fa3597ce16b3af6e63d1
lays.almeida <lays.almeida@compasso.com.br>
2023-09-27 23:36:24 -0300

    add text color and home image

--- projeto_organiza/src/components/Navbar.jsx
+++ projeto_organiza/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             </div>
             <div className={
                 menuOpen
-                    ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                    ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500 text-[#000000]'
                     : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
             }
             >
